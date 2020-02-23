6ea9a63b6f67555b2afe20febc9b8b7c31b2ff87
Cdonegan <46702071+Cdonegan@users.noreply.github.com>
2020-02-22 17:28:46 -0800

    Add files via upload

---
+++ Hw1/HW1/HW1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by Connor Donegan on 1/17/20.
+//  Copyright © 2020 Connor Donegan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Hw1/HW1/QuizApp/QuizApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Connor Donegan on 1/21/20.
+//  Copyright © 2020 Connor Donegan. All rights reserved.
+//
+ // viewdidload is the best place to put code that you want to see when the application loads up
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var flipped: Bool = false
+    var flipped2: Bool?     // <=-- optional type that is initilized to nil
+    
+    func  ino()
+    {
+        flipped = false
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
